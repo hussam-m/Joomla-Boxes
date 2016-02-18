8a3af3bf81e559ab88267c8d7e1080d4da623887
hussam-m <hussam.am@gmail.com>
2016-02-17 20:15:23 -0500

    adding a HTML example to for testing

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,3 +3,34 @@
 defined('_JEXEC') or die;?>
 <?php echo $hello; ?>
 <h1>Testing Boxes</h1>
+
+<div class="row-fluid boxes">
+  <div class="span4">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>S²ERC Brochure</h3>
+        <p>Read our brochure and get a glimpse of what S²ERC is all about.</p>
+        <a href="#" class="btn btn-primary" role="button">Download Brochure</a>
+      </div>
+    </div>
+  </div>
+  <div class="span4">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Who is S2ERC?</h3>
+        <p>S²ERC researchers are faculty and students from CS, EE, Industrial Engineering, Management, Mathematics, and Systems Engineering programs at thirteen different universities...</p>
+        <a href="#" class="btn btn-primary" role="button">Read more</a>
+      </div>
+    </div>
+  </div>
+  <div class="span4">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Next Showcase</h3>
+        <p>Join us at Ball State University May 25-26, 2016 to celebrate 30 years
+as an NSF I/UCRC! </p>
+        <a href="#" class="btn btn-primary" role="button">Register Now</a>
+      </div>
+    </div>
+  </div>
+</div>
