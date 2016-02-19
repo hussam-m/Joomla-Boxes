c91c8dbf5f4588d679456be8c9d2aaa44bb36bbd
hussam-m <hussam.am@gmail.com>
2016-02-18 21:33:11 -0500

    adding and removing some comments

--- mod_joomla-boxes.php
+++ mod_joomla-boxes.php
@@ -1,8 +1,16 @@
 <?php
+/*
+*   Joomla-Boxes is an extanstion for Joomla to show 3 div boxes in the Joomla page
+*   Hussam Almoharb
+*   hussam.am@gmail.com
+*   Last updated on 2/18/2016
+*/
+
 // No direct access
 defined('_JEXEC') or die;
+
+//include helper
 require_once dirname(__FILE__) . '/helper.php';
 
-//$hello = Boxes::info($params); for testing
+// set the layout
 require JModuleHelper::getLayoutPath('mod_joomla-boxes',$params->get('layout', 'default'));
-//echo '2';
